05ce41e77c43a35e66e4786657b193673ba8bab8
thelastnicko <thelastnicko@gmail.com>
2024-02-23 12:23:40 -0500

    add more styles to ticket card

--- app/(components)/TicketCard.js
+++ app/(components)/TicketCard.js
@@ -12,7 +12,11 @@ const TicketCard = () => {
           <DeleteBlock />
         </div>
       </div>
-
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        This is the ticket description, please do this ticket!
+      </p>
       <ProgressDisplay />
       <StatusDisplay />
     </div>
